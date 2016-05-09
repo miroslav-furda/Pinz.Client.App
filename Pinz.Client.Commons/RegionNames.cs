29362b350a3bdaff7da20b3fe2578cbed9cd5348
Miroslav Furda <miroslav.furda@senacor.com>
2016-05-09 08:04:59 +0200

    refactoring -split

--- Pinz.Client.Commons/RegionNames.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Com.Pinz.Client.Commons
-{
-    public static class RegionNames
-    {
-        public const string PinzMainRegion = "PinzMainRegion";
-
-        public const string MainContentRegion = "MainContentRegion";
-    }
-}
--- Pinz.Client.RemoteServiceConsumer/Callback/IServiceRunningIndicator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Com.Pinz.Client.RemoteServiceConsumer.Callback
-{
-    public interface IServiceRunningIndicator
-    {
-        bool IsServiceRunning { get; set; }
-
-    }
-}
