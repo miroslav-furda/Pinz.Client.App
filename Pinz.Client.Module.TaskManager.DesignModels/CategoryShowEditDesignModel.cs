d83f56174549f3a78449d3abd960cc7ffa9358d0
Miroslav Furda <miroslav.furda@senacor.com>
2016-07-29 09:20:02 +0200

    MahApps styling

--- Pinz.Client.Module.TaskManager.DesignModels/CategoryShowEditDesignModel.cs
+++ Pinz.Client.Module.TaskManager.DesignModels/CategoryShowEditDesignModel.cs
@@ -10,7 +10,7 @@ namespace Com.Pinz.Client.Module.TaskManager.DesignModels
 
         public CategoryShowEditDesignModel()
         {
-            IsEditorEnabled = false;
+            IsEditorEnabled = true;
             Category = new Category()
             {
                 Name = "Category"
--- Pinz.Client.Wpf.App/Shell.xaml.cs
+++ Pinz.Client.Wpf.App/Shell.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Ninject;
+﻿using MahApps.Metro.Controls;
+using Ninject;
 using System.Windows;
 
 namespace Com.Pinz.Client.Wpf.App
@@ -6,7 +7,7 @@ namespace Com.Pinz.Client.Wpf.App
     /// <summary>
     /// Interaction logic for Shell.xaml
     /// </summary>
-    public partial class Shell : Window
+    public partial class Shell : MetroWindow
     {
         [Inject]
         public Shell(ShellModel model)
